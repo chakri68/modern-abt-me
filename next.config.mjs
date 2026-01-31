40b7e9a12b651c70422032e7f75fb86eb8a4fcaf
chakri68 <chakridevireddy69@gmail.com>
2026-02-01 05:04:56 +0530

    Changes to make github pages work

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+  basePath: process.env.NODE_ENV === "production" ? "/modern-abt-me" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/modern-abt-me" : "",
+};
 
-export default nextConfig
+export default nextConfig;
